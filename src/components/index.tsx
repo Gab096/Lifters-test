fe28945463489e9df348e5be0f73f163408d8132
Gab096 <Gabrielnogueira0000@hotmail.com>
2022-08-04 10:44:27 -0300

    feat: header feito

---
+++ src/components/index.tsx
@@ -0,0 +1,3 @@
+export { default as Navbar } from "./molecules/Navbar";
+export { default as Header } from "./organisms/Header/Header";
+export { default as Banner } from "./molecules/Banner/Banner";
---
+++ src/components/molecules/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar border-bottom  border-secondary">
+      <div className="container">
+        <span className="navbar-brand text-light mb-0  h1">Lifters Shop</span>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
