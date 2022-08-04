0636bbccf7a6862d8c5d83f9c8217968513d5e6c
Gab096 <Gabrielnogueira0000@hotmail.com>
2022-08-04 11:26:14 -0300

    fix: pastas do header ajustadas

--- src/components/molecules/Banner.tsx
+++ src/components/molecules/Banner.tsx
@@ -5,7 +5,7 @@ const Banner = () => {
       <p className="col-4 text-justify">
         Revamp your style with the latest designer trends in men’s clothing or
         achieve a perfectly curated wardrobe thanks to our line-up of timeless
-        pieces.
+        pieces .
       </p>
     </div>
   );
